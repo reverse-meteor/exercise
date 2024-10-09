19f3cec679f9603412cff3888cb36160eb25e8bf
Choy-OH <Oscaryy1009@gmail.com>
2024-10-09 22:35:14 +0800

    Update HumanResourceMachine(1).cpp

--- HumanResourceMachine(1).cpp
+++ HumanResourceMachine(1).cpp
@@ -4,8 +4,8 @@
 #include <cstring>
 #include <string>
 #include <iomanip>
-
-std::string level_name[4] = {"收发室", "sub走廊", "平等化室", "自由关卡"}; // 存储关卡名字，第四关到时候改一下
+using namespace std;
+string level_name[4] = {"收发室", "sub走廊", "平等化室", "自由关卡"}; // 存储关卡名字，第四关到时候改一下
 
 int checkPlayProgress();  // 检查历史最高游玩层数（打算之后每次刷新层数，就把那个文件覆盖掉，输入新的值）
 int chooseYourLevel(int); // 判断玩家选择层数是否合法
@@ -18,10 +18,14 @@ void task2();
 void task3();
 void task4();
 
+void printf_red(const char *s);
+void printf_green(const char *s);
+void printf_yellow(const char *s);
+
 int main()
 {
-    std::cout << "Human Resource Machine\n";
-
+    printf_yellow("Human Resource Machine\n");
+    std::cout<<"\n======================================================\n\n";
     int highest_level = checkPlayProgress();
 
     int now_level = chooseYourLevel(highest_level);
@@ -68,11 +72,11 @@ int chooseYourLevel(int highest_level)
     {
         if ((choosing_level != 1 && choosing_level != 2 && choosing_level != 3 && choosing_level != 4))
         {
-            cout << "关卡信息输入有误，请重新输入\n";
+            printf_red( "关卡信息输入有误，请重新输入\n");
         }
         else
         {
-            cout << "您暂且无法选择对应关卡,请重新输入\n";
+            printf_red( "您暂且无法选择对应关卡,请重新输入\n");
         }
         cin >> choosing_level;
     }
@@ -84,12 +88,11 @@ void printLevelName(int level)
     using namespace std;
     for (int i = 0; i < 4; i++)
     {
-        cout << "关卡" << i+1;
-        cout << setw(15) << setfill('-') << level_name[i]; // 这里对齐是按所占字节对齐，中文占了两个字节，但是显示没占到两格的宽度，所以最后结果没有对齐
+        cout << "关卡" << i+1 << right << setw(20)<< level_name[i]; // 这里对齐是按所占字节对齐，中文占了两个字节，但是显示没占到两格的宽度，所以最后结果没有对齐
         if (i <= level)
-            printf("  您可以选择\n"); // 感觉这个可以优化一下
+            printf_green("  您可以选择\n"); // 感觉这个可以优化一下
         else
-            printf("  您不可以选择\n");
+            printf_red("  您不可以选择\n");
     }
     return;
 }
@@ -100,7 +103,8 @@ int continueToPlay(){
     cout << "请选择操作：\n"<< "下一关   输入0\n"<< "回到目录 输入1\n"<< "结束游戏 输入2\n";
     cin >> continue_play;
     while(continue_play!=0&&continue_play!=1&&continue_play!=2){
-        cout<<"请输入正确的操作：\n"<< "下一关   输入0\n"<< "回到目录 输入1\n"<< "结束游戏 输入2\n";
+        printf_red("请输入正确的操作：\n");
+        printf("下一关   输入0\n回到目录 输入1\n结束游戏 输入2\n");
         cin >> continue_play;
     }
     if(continue_play==2){
@@ -130,3 +134,20 @@ void task3(){
 void task4(){
 
 }
+
+void printf_red(const char *s)
+{
+    printf("\033[0m\033[1;31m%s\033[0m", s);
+}
+
+void printf_green(const char *s)
+{
+    printf("\033[0m\033[1;32m%s\033[0m", s);
+}
+
+void printf_yellow(const char *s)
+{
+    printf("\033[0m\033[1;33m%s\033[0m", s);
+}
+
+

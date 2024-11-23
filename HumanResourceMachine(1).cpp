221fbef5850f5b1c61203b50a6d770d967308ce7
Choy-OH <Oscaryy1009@gmail.com>
2024-11-23 17:57:05 +0800

    Update HumanResourceMachine(1).cpp

--- HumanResourceMachine(1).cpp
+++ HumanResourceMachine(1).cpp
@@ -5,6 +5,8 @@
 #include <cstring>
 #include <vector>
 #include <ctime>
+#include <conio.h>
+
 
 using namespace std;
 void printf_red(const char* s);
@@ -75,26 +77,41 @@ public:
         }
     }
     void showInOut() {
-        
+
     }
-    void showBlock(char num,int x1,int y1)
+    void showBlock(char num, int x1, int y1)
     {
         gotoxy(x1, y1);
-        cout << "+---+" ;
-        gotoxy(x1, y1+1);
-        cout << "| " << num << " |" ;
-        gotoxy(x1, y1+2);
-        cout << "+---+" ;
+        cout << "+---+";
+        gotoxy(x1, y1 + 1);
+        cout << "| " << num << " |";
+        gotoxy(x1, y1 + 2);
+        cout << "+---+";
     }
-    void showRobot(bool carry,int x1,char carry_num)
+    void showRobot(bool carry, int x1, char carry_num)
     {
+        for (int i = 10; i <= 18; i++) {
+            for (int j = x1+5; j < 34; j++) {
+                gotoxy(j, i);
+                cout << ' ';
+            }
+        }
+        for (int i = 10; i <= 18; i++) {
+            for (int j = 6; j < x1; j++) {
+                gotoxy(j, i);
+                cout << ' ';
+            }
+        }
         if (carry) {//扛着东西  
             showBlock(carry_num, x1, 10);
+            gotoxy(x1, 13);
+            cout << "@   @";
         }
+        
         gotoxy(x1, 14);
-        cout << "-----" ;
+        cout << "-----";
         gotoxy(x1, 15);
-        cout << "|@ @|" ;
+        cout << "|@ @|";
         gotoxy(x1, 16);
         cout << "  +  " << endl;
         gotoxy(x1, 17);
@@ -109,8 +126,18 @@ public:
         HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
         SetConsoleCursorPosition(hOut, pos);
     }
+    void getInstructs(int n, vector<string>& instructs) {//操作输入
+        string input;
+        getline(cin, input);
+        for (int i = 1; i <= n; i++) {
+            gotoxy(57, 9 + i);
+            cout << i << ' ';
+            getline(cin, input);
+            instructs.push_back(input);
+        }
+    }
     virtual ~base_task() {};
-    
+
 public:
     int finish_level = 0; // 记录当前关卡方便与highest_level进行比较
 };
@@ -118,10 +145,84 @@ public:
 class task01 : virtual public base_task
 {
 public:
+    char instructInbox(char onhand, vector<char>& inbox) {//inbox操作
+        
+        Sleep(500);
+        for (int i = 9; i >= 6; i--) {
+            showRobot(0, i, '0');
+            Sleep(500);
+        }
+        onhand = inbox[0];
+        inbox.erase(inbox.begin());
+        
+        showRobot(1, 6, onhand);
+        for (int i = 0; i < 6; i++) {//去除箱子所在位置的内容
+            gotoxy(0, 8 + i);
+            cout << "     ";
+        }
+
+        Sleep(500);
+        printInbox(inbox);
+        Sleep(500);
+
+        for (int i = 6; i <= 10; i++) {
+            showRobot(1, i, onhand);
+            Sleep(500);
+        }
+        return onhand;
+    }
+    char instructOutbox( char onhand,vector<char>& outbox) {//outbox操作
+        Sleep(500); 
+        for (int i = 11; i <=29; i++) {
+            showRobot(1, i, onhand);
+            Sleep(500);
+        }
+        outbox.push_back(onhand);
+        onhand = 'X';
+        showRobot(0, 29, 'X');
+        printOutbox(outbox);
+        for (int i = 0; i < 6; i++) {//去除箱子所在位置的内容
+            gotoxy(29, 8 + i);
+            cout << "     ";
+        }
+        Sleep(500);
+
+        for (int i = 28; i >= 10; i--) {
+            showRobot(0, i, 'X');
+            Sleep(500);
+        }
+        return onhand;
+    }
+
+    int doInstructs(int n, vector<string>& instructs,vector<char>& inbox, vector<char>& outbox) {//操作执行
+        char on_hand= 'X';
+        for (int i = 1; i <= n; i++) {
+            gotoxy(55, 9 + i);
+            cout << "->";
+            if (instructs[i-1] == "inbox") {
+                if (inbox.size() == 0) return i;//防止访问越界
+                on_hand = instructInbox(on_hand,inbox);
+                
+            }
+            else if (instructs[i-1] == "outbox") {
+                if (on_hand < '0' || on_hand>'9') return i;
+                else {
+                    on_hand=instructOutbox(on_hand, outbox);
+                }
+            }
+            else {
+                return i;
+            }
+            gotoxy(55, 9 + i);
+            cout << "  ";
+        }
+        return 0;
+    }
     void dotask()
     {
         printf_yellow("您当前游玩的关卡是: 收发室");
         Sleep(2000);
+        next:
         system("cls");
         srand(time(0));
         char num[3];
@@ -133,51 +234,74 @@ public:
             inbox.push_back(num[i]);
         }
         vector<char> outbox;
+        vector<string> instructs;
         // 关卡内容
         cout << "欢迎新员工！这是你的第一天" << endl;
         cout << "关卡信息:让机器人取出输入序列(Inbox)上的每个积木放入输出序列(Outbox)中" << endl;
         cout << "可用空地数:0" << endl;
         cout << "可用指令集:inbox,outbox" << endl;
-        printScreen(inbox,outbox);
+        printScreen(inbox, outbox);
         
-        
-        // if success
-        int n;
+        int n;//输入的操作数目
         cin >> n;
-        printf_green("挑战成功！");
+        getInstructs( n, instructs);
+        int error_on=doInstructs(n, instructs, inbox, outbox);
+
+        if (error_on != 0||outbox.size()!=3) {//编译错误，程序没跑完或者outbox中数不为3个
+            gotoxy(82, 15);
+            if (error_on != 0) {//编译错误，程序没跑完
+                printf_red("Error");
+                gotoxy(73, 16);
+                cout << "on line" << error_on;
+            }
+            else if (outbox.size() < 3) printf_red("Fail");//outbox中数不为3个,结果一定错误
+            gotoxy(73, 17);
+            cout << "按下\"Enter\"重新开始";
+            bool waitForEnter = 1;
+            while (waitForEnter) {
+                if (_kbhit()) {
+                    char ch = _getch();
+                    if (ch == '\r') {
+                        waitForEnter = 0;
+                    }
+                }
+            }
+            goto next;
+        }
+
+        for (int i = 0; i < 3; i++) {//outbox有3个数时
+            if (outbox[i] != num[i]) {
+                gotoxy(82, 15);
+                printf_red("Fail");
+                gotoxy(73, 17);
+                cout << "按下\"Enter\"重新开始";
+                bool waitForEnter = 1;
+                while (waitForEnter) {
+                    if (_kbhit()) {
+                        char ch = _getch();
+                        if (ch == '\r') {
+                            waitForEnter = 0;
+                        }
+                    }
+                }
+                goto next;
+            }
+        }
+
+        // if success
+        gotoxy(82, 15);
+        printf_green("Success");
         Sleep(2000);
         finish_level = 1;
         system("cls"); // 判断是否成功，到main函数实现
     }
-    void instructInbox(char num) {
 
-    }
-    void instructOutbox(char num) {
+    void printScreen(vector<char>& inbox, vector<char>& outbox) {//打印游戏界面
+        printInbox(inbox);
 
-    }
-    
-    void printScreen(vector<char> &inbox, vector<char> &outbox) {
-        gotoxy(0, 8);
-        cout << "Inbox"<<endl<<endl;
-        int inbox_size = inbox.size();
-        for (int i = 0; i < inbox_size; i++) {
-            showBlock(inbox[i], 0, 10 + i * 3);   
-        }
-        for (int i = inbox_size; i < 3; i++) {
-            showBlock('X', 0, 10 + i * 3 );
-        }
         showRobot(0, 10, '0');
 
-        gotoxy(34, 8);
-        cout << "Outbox";
-        int outbox_size = outbox.size();
-        for (int i = 0; i < outbox_size; i++) {
-            showBlock(outbox[i], 34, 10+i*3);
-        }
-
-        for (int i = outbox_size; i < 3; i++) {
-            showBlock('X', 34, 10 + i * 3);
-        }
+        printOutbox(outbox);
 
 
         for (int i = 0; i < 20; i++) {
@@ -186,25 +310,72 @@ public:
             gotoxy(53, 8 + i);
             cout << '|';
         }
+
         gotoxy(43, 8);
         cout << "可用代码";
-        gotoxy(43,10);
-        cout << "Inbox";
+        gotoxy(43, 10);
+        cout << "inbox";
         gotoxy(43, 12);
-        cout << "Outbox";
+        cout << "outbox";
 
+        for (int i = 1; i < 10; i++) {
+            gotoxy(72, 8 + i);
+            cout << '|';
+            gotoxy(93, 8 + i);
+            cout << '|';
+        }
+        gotoxy(72, 8);
+        cout << "+--------------------+";
+        gotoxy(73, 9);
+        cout << "帮助：" ;
+        gotoxy(73, 10);
+        cout << "第0行输入一个数字n";
+        gotoxy(73, 11);
+        cout << "表示指令总数为n";
+        gotoxy(73, 12);
+        cout << "接下来n行输入n条指令";
+        gotoxy(73, 13);
+        cout << "请以小写字母输入";
+        gotoxy(73, 14);
+        cout << "--------------------";
+        gotoxy(73, 15);
+        cout << "游戏结果:";
+        gotoxy(72, 18);
+        cout << "+--------------------+";
 
         gotoxy(55, 7);
-        cout << "你的代码";
-        gotoxy(72, 8);
-        cout << "运行";
-        gotoxy(72, 10);
-        cout << "撤回";
+        cout << "你的代码:";
         gotoxy(55, 8);
         cout << "=====CODE=====";
+        gotoxy(57, 9);
+        cout << "0 ";
         /*gotoxy()*/
     }
 
+    void printInbox(vector<char>& inbox) {//打印inbox
+        gotoxy(0, 8);
+        cout << "Inbox" << endl << endl;
+        int inbox_size = inbox.size();
+        for (int i = 0; i < inbox_size; i++) {
+            showBlock(inbox[i], 0, 10 + i * 3);
+        }
+        for (int i = inbox_size; i < 3; i++) {
+            showBlock('X', 0, 10 + i * 3);
+        }
+
+    }
+    void printOutbox(vector<char>& outbox) {//打印outbox
+        gotoxy(34, 8);
+        cout << "Outbox";
+        int outbox_size = outbox.size();
+        for (int i = 0; i < outbox_size; i++) {
+            showBlock(outbox[i], 34, 10 + i * 3);
+        }
+
+        for (int i = outbox_size; i < 3; i++) {
+            showBlock('X', 34, 10 + i * 3);
+        }
+    }
 };
 
 class task02 : virtual public base_task
